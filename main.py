1df0bd02acc694f9e7f4e43af2607e73c292fd2c
Alma <abecerrilsalas@gmail.com>
2021-09-20 00:47:22 -0700

    added more hamlet

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pytest
-
+# “Whether 'tis nobler in the mind to suffer
+# The slings and arrows of outrageous fortune,
+# Or to take arms against a sea of troubles,
+# And by opposing end them?”
 
 def always_returns_true():
     return True
